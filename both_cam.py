c98716ef24799c5ce83038ba1b55a26940605060
BlackBoxPI <ablaizot@bu.edu>
2024-10-25 17:31:15 -0400

    both c

---
+++ both_cam.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+from multiprocess import Process
+
+def increment_filename(filepath):
+    base, ext = os.path.splitext(filepath)
+    counter = 1
+    new_filepath = filepath
+    while os.path.exists(new_filepath):
+        new_filepath = f"{base}_{counter}{ext}"
+        counter += 1
+    return new_filepath
+
+def see_cam():
+    output_file = increment_filename("/home/coolhippo159/Videos/see_cam.mjpeg")
+    see_cam_cmd = f"sudo v4l2-ctl --device /dev/video0 --stream-mmap --stream-to={output_file} --stream-count=1000000 --set-fmt-video=width=1280,height=720,pixelformat=MJPG --set-parm 60"
+    print(see_cam_cmd)
+    subprocess.run(see_cam_cmd, shell=True)
+
+def pi_cam():
+    output_file = increment_filename("/home/coolhippo159/Videos/pi_cam.mjpeg")
+    pi_cam_cmd = f"libcamera-vid --codec mjpeg -t 200000000 -o {output_file}"
+    print(pi_cam_cmd)
+    subprocess.run(pi_cam_cmd, shell=True)
+
+def main():
+    p1 = Process(target=see_cam)
+    p2 = Process(target=pi_cam)
+    p1.start()
+    p2.start()
+
+if __name__ == '__main__':
+    main()
