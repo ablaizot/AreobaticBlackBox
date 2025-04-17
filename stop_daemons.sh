5f4516758563b8f0d1ad14a746914e77ff91437b
ablaizot <ablaizot@bu.edu>
2025-04-16 23:16:53 -0400

    add status to stop

--- stop_daemons.sh
+++ stop_daemons.sh
@@ -2,3 +2,6 @@
 sudo systemctl stop stamp_video.service
 sudo systemctl stop mavproxy.service
 sudo systemctl stop gps_logger.service
+sudo systemctl status stamp_video.service
+sudo systemctl status mavproxy.service
+sudo systemctl status gps_logger.service
