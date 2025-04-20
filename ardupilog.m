6d02fc949538163e03197e81a689b605c5fe98f8
ablaizot <ablaizot@bu.edu>
2025-04-20 18:34:31 -0400

    4 19 flight

--- ardupilog.m
+++ ardupilog.m
@@ -1,5 +1,6 @@
-load("11_17_24\00000002.BIN-18961732.mat")
+load("4_19_25_data\flight_2\00000075.log-28536934.mat")
 disp("https://ardupilot.org/plane/docs/logmessages.html")
+close all
 gps_status(GPS_0, GPA_0)
 plot_altitudes(GPS_0,BARO_0, BARO_1)
 plot_gps(GPS_0)
@@ -8,17 +9,14 @@ figure
 lat = POS(:,3);
 lon = POS(:,4); 
 plot_AHRS(AHR2)
-% plot_IMU(IMU_0)
-% plot_IMU(IMU_1)
-% plot_IMU(IMU_2)
+plot_IMU(IMU_0)
+plot_IMU(IMU_1)
+plot_IMU(IMU_2)
 
 
 % geoscatter(lat,lon,"filled")
 % geolimits([min(lat) max(lat)],[min(lon) max(lon)])
 % 
-load("11_17_24\00000003.BIN-11150736.mat")
-gps_status(GPS_0, GPA_0)
-plot_altitudes(GPS_0,BARO_0, BARO_1)
 % 
 % figure
 % lat = POS(:,3);
